f9d7b9487ee83eaab605f7636ceac9b3d8a79908
Jenny Shi <jennpeare@gmail.com>
2016-03-27 16:54:27 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adhocbookclubApp
+ * @description
+ * # adhocbookclubApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adhocbookclubApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adhocbookclubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adhocbookclubApp
+ */
+angular.module('adhocbookclubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
